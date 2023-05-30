4188f2a2a54881ab390b8304f42b0a7c803ab459
LucaGelmini <lucagelmini@hotmail.com>
2023-05-30 14:45:46 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>EAVID</h1>
+      <h2>Página en construcción</h2>
+      <p>¡Sea paciente!</p>
+    </>
+  );
+}
+
+export default App;
