1a5003674a845888bbfc943736aa9c0ea1a80370
Luca <lucagelmini@github.com>
2023-05-31 01:37:36 -0300

    custom colors tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./src/**/*.{html,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      'sans': ['Arimo']
+    },
+    extend: {
+      colors: {
+        eavid: {
+          100: '#C4E1D8',
+          200: '#89C3B1',
+          300: '#6CB49E',
+          400: '#4EA58A',
+          500: '#309676',
+          600: '#128662',
+        }
+      }
+    },
   },
   plugins: [],
 }
