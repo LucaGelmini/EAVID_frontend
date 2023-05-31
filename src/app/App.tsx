ceedaed877b202ac85eec3cf6d804890f6fa57e0
LucaGelmini <lucagelmini@hotmail.com>
2023-05-30 21:24:34 -0300

    proyecto vite + tailwind+ config

---
+++ src/app/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import logo from "../assets/Recurso 14@4x.png";
+
+function App() {
+  return (
+    <>
+      <header className=" text-4xl flex justify-around h-16 bg-gray-300">
+        <img src={logo} alt="logo EAVID" />
+        <h1>EAVID Entrenamiento + acompañamiento, vida y deporte</h1>
+      </header>
+      <main className="m-auto w-4/5">
+        <h2>Página en construcción</h2>
+        <p>¡Sea paciente!</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
