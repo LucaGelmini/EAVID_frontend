eff14e4469c7329685bb95687b2ef58cd30f8c97
Luca <lucagelmini@github.com>
2023-07-16 23:59:03 -0300

    falta mandar el mail

--- src/pages/Slug.tsx
+++ src/pages/Slug.tsx
@@ -1,17 +1,22 @@
+import { useState, useEffect } from "react";
 import { useQuery } from "@apollo/client";
 import type { UseQueryResult, PageNode, PostsData } from "../types/queryTypes";
 import pageQuery from "../graphql/pageQuery.graphql";
+import mailMutation from "../graphql/mailMutation.graphql";
 import Main from "../layouts/Main";
 import Spinner from "../ui/Spinner";
 import landingPostsQuery from "../graphql/landingPostsQuery.graphql";
 import LandingPost from "../components/LandingPost";
 import ContactForm from "../components/ContactForm";
+import client from "../graphql/apolloClient.ts";
 
 type Props = {
   databaseId: number;
+  hasContactForm?: boolean | null;
+  contactMail?: string | null;
 };
 
-const Slug = ({ databaseId }: Props) => {
+const Slug = ({ databaseId, hasContactForm, contactMail }: Props) => {
   const { loading, error, data }: UseQueryResult<{ page: PageNode }> = useQuery(
     pageQuery,
     {
@@ -24,16 +29,27 @@ const Slug = ({ databaseId }: Props) => {
     console.error(error);
   }
 
-  const {
-    loading: postsLoading,
-    error: postsError,
-    data: postsData,
-  }: UseQueryResult<PostsData> = useQuery(landingPostsQuery, {
-    variables: { categoryName: data?.page.slug },
-  });
-  if (postsError) {
-    console.error(postsError);
-  }
+  const [postsData, setPostData] = useState<PostsData | undefined>(undefined);
+  useEffect(() => {
+    if (!loading && data !== undefined) {
+      client
+        .query({
+          query: landingPostsQuery,
+          variables: {
+            categoryName: data?.page.slug,
+          },
+        })
+        .then((res) => setPostData(res.data))
+        .catch(console.error);
+    }
+  }, [loading, data]);
+
+  client
+    .mutate({
+      mutation: mailMutation,
+    })
+    .then(console.log)
+    .catch(console.error);
 
   return (
     <Main>
@@ -41,13 +57,15 @@ const Slug = ({ databaseId }: Props) => {
         <Spinner className="w-full" />
       ) : (
         <>
-          <div className="w-3/4 mx-auto p-4">
-            <h1 className="text-3xl font-semibold">{data.page.title}</h1>
-            {data.page.editorBlocks?.map((block) => block.attributes.content)}
-            <ContactForm />
-          </div>
+          {hasContactForm && (
+            <div className="w-3/4 mx-auto p-4">
+              <h1 className="text-3xl font-semibold">{data.page.title}</h1>
+              {data.page.editorBlocks?.map((block) => block.attributes.content)}
+              <ContactForm />
+            </div>
+          )}
           <div>
-            {postsData === undefined || postsLoading ? (
+            {postsData === undefined ? (
               <Spinner className="w-full" />
             ) : (
               <>
